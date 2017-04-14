40094ebc951e92fa28afc980a92335c6c450e0c4
cong196 <vancongct.96@gmail.com>
2017-04-14 14:36:39 +0700

    Add files via upload

---
+++ spring1-master/src/main/java/com/hellokoding/springboot/HelloController.java
@@ -0,0 +1,27 @@
+package com.hellokoding.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+    
+    @RequestMapping("/edit")
+    public String welcome(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "edit";
+    }
+    
+    @RequestMapping("/tagicsse")
+    public String tagicsse(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "icsse17";
+    }
+}
